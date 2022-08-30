6dab761e8846bc8a8137facc5e542739743b0ef2
Christopher Hill <cjameshill@gmail.com>
2022-08-30 11:12:42 +0000

    Upgraded   mem_cache_size: "512m"

--- src/NodePoolMigration.sh
+++ src/NodePoolMigration.sh
@@ -1,3 +1,3 @@
-for node in $(kubectl get nodes -l cloud.google.com/gke-nodepool=kong-pool-v1 -o=name); do
+for node in $(kubectl get nodes -l cloud.google.com/gke-nodepool=pool-v4 -o=name); do
   kubectl drain --force --ignore-daemonsets --delete-emptydir-data --grace-period=10 "$node";
 done
